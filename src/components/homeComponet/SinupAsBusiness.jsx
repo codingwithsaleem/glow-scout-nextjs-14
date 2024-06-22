0182e48d21d38c0f4445cb092fd1e64c56f7dd28
fitsaleem <saleem.raza@qavitechnologies.com>
2024-06-22 20:08:36 +0500

    updates

--- src/components/homeComponet/SinupAsBusiness.jsx
+++ src/components/homeComponet/SinupAsBusiness.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import { ArrowUpRight } from "lucide-react";
-import { Button } from "@/components/ui/Button";
+import { Button } from "@/components/ui/button";
 
 const SinupAsBusiness = () => {
   return (
